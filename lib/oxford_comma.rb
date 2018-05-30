81d52e6cba1fafb773262b5367abfdf8fd63e2e6
Amy Tu <siamesebb@gmail.com>
2018-05-30 21:19:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.size > 2 
-    last_val = arr.pop.prepend("and ")
-    arr.push(last_val)
-    arr.join(", ")
+    last_val = array.pop.prepend("and ")
+    array.push(last_val)
+    array.join(", ")
   else
-    array[0...-1].join(", ")
+    array.join(" and ")
     end
 end
